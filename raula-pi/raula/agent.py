81cf732e1231f8268708f06e1ce08e77d59d8c0d
Julio Faerman <jfaerman@gmail.com>
2019-10-25 11:16:38 +0200

    Improved camera startup

--- raula-pi/raula/agent.py
+++ raula-pi/raula/agent.py
@@ -1,27 +1,56 @@
 import os
 import math
 import time
-from multiprocessing import Process
-from .camera import camera_process
+import json
 
-def start_processes():
+from multiprocessing import Process
+from pathlib import Path
+
+from .camera import camera_probe
+
+
+def start_processes(config):
     print("Starting processes")
     processes = {}
-    processes["camera"] = Process(target=camera_process)
+
+    camera_proc = camera_probe(config)
+    if (camera_proc):
+        processes["camera"] = camera_proc
+
     print("{} processes created".format(len(processes)))
     for process in processes.values():
         process.start()
     return processes
 
-def wait_all(processes = {}):
+
+def wait_all(processes={}):
     print("Waiting for processes to finnish")
     for process in processes.values():
         process.join()
 
+
+def lookup_config():
+    config = {}
+    user_home = Path.home()
+
+    raula_home = user_home / ".raula"
+    config["raula_home"] = str(raula_home)
+
+    raula_videos = raula_home / "videos"
+    config["raula_videos"] = str(raula_videos)
+
+    raula_pictures = raula_home / "pictures"
+    config["raula_pictures"] = str(raula_pictures)
+    print("raula configuration:")
+    print(json.dumps(config, sort_keys=True, indent=2))
+    return config
+
+
 def start():
     print("Starting the ride")
     try:
-        processes = start_processes()
+        config = lookup_config()
+        processes = start_processes(config)
         wait_all(processes)
     except KeyboardInterrupt:
         print("Ride stopped")
--- raula-pi/raula/camera.py
+++ raula-pi/raula/camera.py
@@ -1,19 +1,35 @@
+# Raspbery pi camera module
 import time
-import picamera
+from multiprocessing import Process
 
-def camera_process():
+
+def camera_probe(config):
+    print("Probing for raspi camera")
+    try:
+        import picamera
+        print("Raspi camera found")
+        return Process(target=camera_process, args=(config,))
+    except ModuleNotFoundError:
+        print("Raspi camera not found")
+        return None
+
+
+def camera_process(config):
     print("Camera Starting")
+    import picamera
     video_length = 30
     with picamera.PiCamera() as camera:
-        time.sleep(2) #camera wakeup 
+        time.sleep(2)  # camera wakeup
         try:
             while(True):
                 ts = int(time.time())
                 print("click {}".format(ts))
-                #TODO mkdir first
-                camera.start_recording('../videos/picam_{}.h264'.format(ts))
-                camera.capture('../pictures/picam_{}.jpg'.format(ts), use_video_port=True)
-                #TODO Record faster as accidents may be a glimpse
+                # TODO mkdir first
+                camera.start_recording(
+                    '../videos/picam_{}.h264'.format(ts))
+                camera.capture(
+                    '../pictures/picam_{}.jpg'.format(ts), use_video_port=True)
+                # TODO Record faster as accidents may be a glimpse
                 camera.wait_recording(video_length)
                 camera.stop_recording()
         except KeyboardInterrupt:
